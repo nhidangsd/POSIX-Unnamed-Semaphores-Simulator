ee31b61d9208934f8c8f975004c8c7613e5588a1
Nhi Dang <nhidangsd@gmail.com>
2021-04-10 15:07:02 -0700

    added new struct to mizzo.h

--- mizzo.h
+++ mizzo.h
@@ -20,7 +20,25 @@ typedef struct{
     int E, L, f, e;
 } OPTION_ARGS;
 
+/* Type of operations threads can perform */
+typedef enum{
+    INCREMENT,
+    DECREMENT,
+} OPERATION;
 
+typedef struct{
+    OPERATION Operation; /* Specify what should be done */
+    char* Name;          /* Name of this thread */
+    char* Message;       /* Message to display the current state */
+    int N;               /* Number of times to perform action */
+    sem_t *MutexPtr;     /* pointer to Critical section */
+    int *ValuePtr        /* pointer to shared data */
+} THREAD_DATA;
+
+/* 
+ * processArgs - Scans input arguments and setup the flags for later usage 
+ * @param 
+ */
 void processArgs(int argc, char* argv[], OPTION_ARGS* flags);
 void runSimulation(OPTION_ARGS flag);
 
